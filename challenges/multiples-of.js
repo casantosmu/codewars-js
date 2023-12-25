7934416b633a96d24cee458fc8a90b6d296973e2
Carlos Santos <casantosmu@gmail.com>
2023-12-25 11:43:06 +0100

    Add eqeqeq ESLint rule

--- challenges/multiples-of.js
+++ challenges/multiples-of.js
@@ -7,7 +7,7 @@
 export function solution(number) {
   let sum = 0;
   for (let index = 3; index < number; index++) {
-    if (index % 3 == 0 || index % 5 == 0) {
+    if (index % 3 === 0 || index % 5 === 0) {
       sum += index;
     }
   }
